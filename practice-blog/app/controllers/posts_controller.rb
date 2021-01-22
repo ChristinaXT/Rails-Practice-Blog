841a954e8523f59c12f336333993cfa03e7b16cc
Christina Williams <christinaxtwilliams@gmail.com>
2021-01-21 20:21:07 -0500

    c

--- practice-blog/app/controllers/posts_controller.rb
+++ practice-blog/app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
+  # before_action :find_post, only: [:show, :update, :edit, :destroy]
+
 
   def index
+    @posts = Post.all.order("created_at DESC")
   end
 
   def new
@@ -17,6 +20,30 @@ class PostsController < ApplicationController
     end
 
   end
+
+  def show
+	end
+
+	def update
+
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+
+	end
 
   private
 
