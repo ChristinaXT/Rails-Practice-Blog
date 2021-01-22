ebd240c8804efed761b947d6f05a53fcbfc15a7c
Christina Williams <christinaxtwilliams@gmail.com>
2021-01-21 19:08:36 -0500

    controller

--- practice-blog/app/controllers/posts_controller.rb
+++ practice-blog/app/controllers/posts_controller.rb
@@ -4,12 +4,19 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new()
   end
 
-  
+  private
+
+  def post_params
+    params.require(:post)
+  end
+
 
 
 end
